672c7a18edcccdc46a672f5ccebbed24e256014f
Lobster <92152762+lobsterssss@users.noreply.github.com>
2025-06-11 10:09:24 +0200

    adds unit tests to this repo
    this push will add the unittests from unit_test_circus repo to circus trein repo

---
+++ CircusTrainUnitTests/unit_test_circus/Rules/CartCapacityTests.cs
@@ -0,0 +1,40 @@
+﻿using CircusTrain.Rules;
+using CircusTrain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CircusTrain.Carts;
+
+namespace CircusTrainUnitTests
+{
+    [TestClass]
+    public sealed class CartCapacityTests
+    {
+        [TestMethod]
+        public void CanAddAnimal_AddMediumToFullCart_ReturnFalse()
+        {
+            ICartConstraint rule = new CartCapacity();
+            Cart cart = new Cart([new CartCapacity(), new AnimalBiggerThanLargestCarnivore(), new CarnivoreSmallerThanSmallestAnimal()]);
+            cart.TryToAddAnimal(new Animal(Animal.Size.Large, false));
+            cart.TryToAddAnimal(new Animal(Animal.Size.Large, false));
+
+            bool result = rule.CanAddAnimal(new Animal(Animal.Size.Medium, false), cart);
+
+            Assert.AreEqual(false, result, $"couldn't be added \n {result}");
+        }
+
+        [TestMethod]
+        public void CanAddAnimal_AddMediumToHalfFullCart_ReturnTrue()
+        {
+            ICartConstraint rule = new CartCapacity();
+            Cart cart = new Cart([new CartCapacity(), new AnimalBiggerThanLargestCarnivore(), new CarnivoreSmallerThanSmallestAnimal()]);
+            cart.TryToAddAnimal(new Animal(Animal.Size.Large, false));
+
+            bool result = rule.CanAddAnimal(new Animal(Animal.Size.Medium, true), cart);
+
+            Assert.AreEqual(true, result, $"couldn't be added \n {result}");
+        }
+    }
+}
---
+++ CircusTrainUnitTests/unit_test_circus/Rules/ExperimentalCartCapacityTests.cs
@@ -0,0 +1,41 @@
+﻿using CircusTrain.Rules;
+using CircusTrain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CircusTrain.Carts;
+
+namespace CircusTrainUnitTests
+{
+    [TestClass]
+    public sealed class ExperimentalCartCapacityTests
+    {
+        [TestMethod]
+        public void CanAddAnimal_AddMediumToFullExperimentalCart_ReturnTrue()
+        {
+            ICartConstraint rule = new ExperimentalCartCapacity();
+            ExperimentalCart cart = new ExperimentalCart([new ExperimentalCartMaxSize(), new ExperimentalCartCapacity()]);
+            cart.TryToAddAnimal(new Animal(Animal.Size.Small, false));
+            cart.TryToAddAnimal(new Animal(Animal.Size.Small, false));
+
+            bool result = rule.CanAddAnimal(new Animal(Animal.Size.Medium, true), cart);
+
+            Assert.AreEqual(false, result, $"couldn't be added \n {result}");
+        }
+
+        [TestMethod]
+        public void CanAddAnimal_AddMediumToHalfFullExperimentalCart_ReturnFalse()
+        {
+            ICartConstraint rule = new ExperimentalCartCapacity();
+            ExperimentalCart cart = new ExperimentalCart([new ExperimentalCartMaxSize(), new ExperimentalCartCapacity()]);
+            cart.TryToAddAnimal(new Animal(Animal.Size.Medium, false));
+
+            bool result = rule.CanAddAnimal(new Animal(Animal.Size.Medium, true), cart);
+
+            Assert.AreEqual(true, result, $"couldn't be added \n {result}");
+        }
+
+    }
+}
